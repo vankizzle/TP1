16e800459eed51801e1ad1a2562d077c0f299008
vankizzle <you@example.com>
2017-10-10 12:12:37 -0700

    working v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,48 @@ class ApplicationController < ActionController::Base
 		render plain: ('%.2f'%sum);
 
 	end
+	
+	def filter
+		sum = 0
+
+		CSV.foreach(params['file'].path) do |row|
+			if row[2].to_i % 2!=0 
+       				cdsum+=row[1].to_i
+			end
+		end
+		
+		sum = sum.ceil 
+		render plain: ('%.2f'%sum);
+	end
+	
+	def intervals
+		
+	file = CSV.parse(params["file"].read, converters: :numeric)
+	len = file.length - 30
+	couter = 0
+	maxSum = 0
+		while counter <= len do
+			count = 0
+			sum = 0
+			arr.each do |row|
+				
+				if count >= counter
+					sum += row[0]
+				end
+				count += 1
+				
+				if count == 30 + counter
+					break
+				end
+			end
+			if maxSum <= sum
+				maxSum = sum
+			end
+			counter += 1
+		end 
+		render plain: ('%.2f'%maxSum);
+end
+		
+
+	end
 end
